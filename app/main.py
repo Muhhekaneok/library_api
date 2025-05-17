77d69c406652fb1c672835503f280528a189926a
wizardrad@gmail.com <wizardrad@gmail.com>
2025-05-17 14:14:16 +0500

    Little hardcode for test Get/books

--- app/main.py
+++ app/main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Library API is running"}
+
+
+@app.get("/books")
+def get_books():
+    return [
+        {"id": 1, "title": "Three men in a boat", "author": "Gerome K. Jerome"},
+        {"id": 2, "title": "Spin Dictators", "author": "Sergey Guriev"},
+        {"id": 3, "title": "Изучаем Python", "author": "Eric Matthes"},
+        {"id": 4, "title": "Статистика и котики", "author": "Владимир Савельев"}
+    ]
